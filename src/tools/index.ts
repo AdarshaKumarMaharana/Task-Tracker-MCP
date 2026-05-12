79b200be655caa4c63ee1200db77bb1c749b89c9
Adarsha kumar Maharana <eadarsha2003@gmail.com>
2026-05-12 15:29:27 +0530

    Push from ForgeSphere MCP wizard

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,2 @@
 // Re-export all tool handlers.
 export { create_taskHandler } from "./create_task.js";
-export { list_tasksHandler } from "./list_tasks.js";
-export { complete_taskHandler } from "./complete_task.js";
